26cf3522112e6f5cdd3f811e949fd381a2e77070
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-06-06 20:22:37 +0200

    Esercizio quasi finito ma va decisamente corretto

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ createApp({
 
                 {
                     'text' : 'Task2' ,
-                    'done' : true
+                    'done' : false
 
                 },
 
@@ -24,7 +24,7 @@ createApp({
 
                 {
                     'text' : 'Task4' ,
-                    'done' : true
+                    'done' : false
 
                 },
 
@@ -32,12 +32,76 @@ createApp({
                     'text' : 'Task5' ,
                     'done' : true
 
+                },
+
+                {
+                    'text' : 'Task6' ,
+                    'done' : false
+
+                },
+
+                {
+                    'text' : 'Task7' ,
+                    'done' : true
+
+                },
+
+                {
+                    'text' : 'Task8' ,
+                    'done' : false
+
+                },
+
+                {
+                    'text' : 'Task9' ,
+                    'done' : true
+
+                },
+
+                {
+                    'text' : 'Task10' ,
+                    'done' : false
+
                 }
-            ]
+            ],
+            
+            isTaskDone : '',
+            newElement: {
+                'text' : '' ,
+                'done' : true
+
+            }
         }
     },
 
     methods: {
+        isTaskDoneFunction(index){
+            if(this.todoList[index].done === true) {
+                this.isTaskDone = true,
+                console.log(this.isTaskDone)
+            } else {
+                this.isTaskDone = false,
+                console.log(this.isTaskDone)
+            }
+        },
+
+        removeTask(todoIndex){
+            if(todoIndex >= this.todoList.lenght || todoIndex < 0){
+                console.warn('Error')
+
+            } else {
+                this.todoList.splice(todoIndex, 1)
+            }
+
+        },
+
+        addNewElement(newItem){
+            if(this.newElement === ''){
+
+            }
+            this.todoList.push(newItem);
+            this.newElement = ''
+        }
 
     }
 
